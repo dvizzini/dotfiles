40e72fca0956ac4b73e7f9188de878b50fa65bc5
Daniel Vizzini <dvizzini@CRMBP0415F.local>
2015-06-01 16:51:42 -0400

    bunch of stuff

--- symlinkit.sh
+++ symlinkit.sh
@@ -9,17 +9,26 @@ fi
 
 if [ "$2" = "mac" ]
 then
-    cp .mac_profile ~/.profile
+    cat .mac_profile .profile_footer > ~/.profile
 else
-    cp .profile ~/.profile
+    cp .linux_profile .profile_footer > ~/.profile
     ln -sn $PWD/.dircolors ~/.dircolors
+    ln -sn $PWD/.gitignore_global ~/.gitignore_global
 fi
 
 echo "export PS1=\"\e[01;32m\]$1:\[\e[01;34m\]\w\[\e[01;35m\]\[\033[00m\] $ \"" >> ~/.profile
 
 ln -sn $PWD/.vim ~/.vim
 ln -sn $PWD/.vimrc ~/.vimrc
-ln -sn $PWD/.gitignore_global ~/.gitignore_global
 ln -sn $PWD/.screenrc ~/.screenrc
 
-echo "Be sure to run \`. ~/.profile\` from your shell."
+#git config
+git config --global core.autocrlf false
+git config --global user.name "Daniel Vizzini"
+git config --global user.email dvizzini@contextrelevant.com
+git config --global core.excludesfile .gitignore_global
+git config --global alias.lg "log --all --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --"
+git config --global alias.glol "log --all --color --graph --pretty=format:'%Cred%h%Creset%x09%C(yellow)%d%Creset %s %C(cyan)(%cr) %C(blue)[%an]%Creset' --abbrev-commit"
+git config --global color.ui "diff --color-words"
+
+echo "Be sure to run \`. ~/.profile\` from your shell." 1>&2
