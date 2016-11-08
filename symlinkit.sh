4a62ce849323c85b41d0ba761a7c72785eb8e595
Daniel Vizzini <daniel.vizzini@mlb.com>
2016-11-08 09:56:39 -0500

    freed up ^s, fixed pip, updated email

--- symlinkit.sh
+++ symlinkit.sh
@@ -26,12 +26,22 @@ echo "export PS1=\"\e[01;32m\]$1:\[\e[01;34m\]\w\[\e[01;35m\]\[\033[00m\] $ \"" 
 # git config
 git config --global core.autocrlf false
 git config --global user.name "Daniel Vizzini"
-git config --global user.email dvizzini@contextrelevant.com
+git config --global user.email daniel.vizzini@mlb.com
 git config --global core.excludesfile "~/.gitignore_global"
 git config --global alias.lg "log --all --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --"
 git config --global alias.glol "log --all --color --graph --pretty=format:'%Cred%h%Creset%x09%C(yellow)%d%Creset %s %C(cyan)(%cr) %C(blue)[%an]%Creset' --abbrev-commit"
 git config --global alias.df "diff --color-words"
 git config --global color.ui "true"
+
+# get pip
+if [ "$2" = "mac" ]
+then
+    # get brew
+    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    sudo easy_install pip
+else
+    sudo apt-get -y install python-pip
+fi
 
 # get flake8 for sytastic
 sudo pip install flake8
